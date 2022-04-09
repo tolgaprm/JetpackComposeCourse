2faa1a4d8f63d50a28b21a3bc572c1f937ca0e8a
Tolga Pirim <tolgapirim25@gmail.com>
2022-04-09 11:02:46 +0300

    created a basic layout with Row

---
+++ CoreUIElements/app/src/main/java/com/prmto/coreuielements/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.prmto.coreuielements
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+
+@Composable
+fun MainScreen() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row {
+            Surface(
+                color = Color.Yellow,
+                modifier = Modifier
+                    .height(600.dp)
+                    .width(60.dp)
+            ) {
+
+            }
+            Surface(
+                color = Color.Blue,
+                modifier = Modifier
+                    .height(600.dp)
+                    .width(60.dp)
+            ) {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainScreen()
+}
