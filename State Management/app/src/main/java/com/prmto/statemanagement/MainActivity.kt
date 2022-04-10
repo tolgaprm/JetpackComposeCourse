b074c70a4741c9d374911ae346b234e366e2f018
Tolga Pirim <tolgapirim25@gmail.com>
2022-04-10 17:29:41 +0300

    lifting up state(State Hoisting)

--- State Management/app/src/main/java/com/prmto/statemanagement/MainActivity.kt
+++ State Management/app/src/main/java/com/prmto/statemanagement/MainActivity.kt
@@ -8,45 +8,69 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
-import com.prmto.statemanagement.ui.theme.StateManagementTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           MainScreen()
+            MainScreen()
         }
     }
 }
+
 @Composable
 fun MainScreen() {
+    val greetingListState = remember { mutableStateListOf<String>("Emre", "Mustafa") }
+    var newNameStateContent = remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        GreetingList()
+        GreetingList(
+            nameList = greetingListState,
+            buttonClick = {
+                greetingListState.add(newNameStateContent.value); newNameStateContent.value = ""
+            },
+            textFieldValue = newNameStateContent.value,
+            textFieldUpdate = {newName->
+                newNameStateContent.value = newName
+            }
+        )
     }
 }
 
 @Composable
-fun GreetingList() {
-    val greetingListState = remember { mutableStateListOf<String>("John", "Amanda") }
-    for(name in greetingListState) {
+fun GreetingList(
+    nameList: List<String>,
+    buttonClick: () -> Unit,
+    textFieldValue: String,
+    textFieldUpdate: (newName: String) -> Unit
+) {
+
+    for (name in nameList) {
         Greeting(name = name)
     }
 
-    Button(onClick = { greetingListState.add("Michael") }) {
-        Text("Add new name")
+
+
+    TextField(value = textFieldValue, onValueChange = textFieldUpdate)
+
+
+    Button(onClick = buttonClick) {
+        Text("Add a new name")
     }
 }
 
