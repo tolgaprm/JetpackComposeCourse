fbf3cfe0c260778f29c98da8207151adf58c4bf1
Tolga Pirim <tolgapirim25@gmail.com>
2022-04-22 21:59:39 +0300

    implemented expanding list items

--- MealzApp/app/src/main/java/com/prmto/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ MealzApp/app/src/main/java/com/prmto/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,15 +1,22 @@
 package com.prmto.mealzapp.ui.meals
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -33,6 +40,9 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealCategory(meal: MealsResponse) {
+
+    var isExpanded by remember { mutableStateOf(false) }
+
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = 8.dp,
@@ -42,7 +52,14 @@ fun MealCategory(meal: MealsResponse) {
 
     ) {
 
-        Row() {
+        Row(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+        ) {
 
             AsyncImage(
                 model = meal.imageUrl,
@@ -50,17 +67,36 @@ fun MealCategory(meal: MealsResponse) {
                 modifier = Modifier
                     .size(88.dp)
                     .padding(4.dp)
+                    .align(Alignment.CenterVertically)
             )
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
+                    .fillMaxWidth(0.8f)
                     .padding(16.dp)
             ) {
                 Text(
                     text = meal.name,
                     style = MaterialTheme.typography.h6
                 )
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        text = meal.description,
+                        style = MaterialTheme.typography.subtitle2,
+                        textAlign = TextAlign.Start,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = if (isExpanded) 15 else 4
+                    )
+                }
             }
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(if (isExpanded) Alignment.Bottom else Alignment.CenterVertically)
+                    .clickable { isExpanded = !isExpanded }
+            )
         }
     }
 }
@@ -70,6 +106,6 @@ fun MealCategory(meal: MealsResponse) {
 @Composable
 fun DefaultPreview() {
     MealzAppTheme {
-       MealCategory(meal = MealsResponse("1","Beef","beef",""))
+        MealCategory(meal = MealsResponse("1", "Beef", "beef", ""))
     }
 }
